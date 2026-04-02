bb0606c8e0778cf9ad30ede6fc73323987439e3c
Setayesh <sanavisetayesh@gmail.com>
2026-04-02 11:26:00 -0400

    Update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/license_writer/",
+  base: "/license-writer/",
   plugins: [react()],
   resolve: {
     alias: {
